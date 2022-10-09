53a1fe64c747f141c5c57ae8827059fddeabb963
Vanya <vanya.danilov.95@bk.ru>
2022-10-09 17:40:12 +0500

    homework2(task4)

---
+++ Homework2/Task4.py
@@ -0,0 +1,12 @@
+#Реализуйте алгоритм перемешивания списка.
+
+import random
+
+
+list = [1, 2, 3, 4, 5]
+print ('Исходный список :', list)
+
+for i in range(len(list)-1, 0, -1):
+     j = random.randint(0, i + 1) 
+list[i], list[j] = list[j], list[i] 
+print ('перемешаный список : ', list)
