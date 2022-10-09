0f1e605166509538546c3f16c8d3857ea9582081
Vanya <vanya.danilov.95@bk.ru>
2022-10-09 17:20:30 +0500

    homework2(task3)

--- Homework2/Task3.py
+++ Homework2/Task3.py
@@ -2,3 +2,22 @@
 #Пример:
 #Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 
+
+def Sum_num(num):
+    return num.isdigit()
+
+num = None
+
+while not  Sum_num(str(num)):
+    num = input("Введите число n = ")
+num = int(num) 
+
+result = dict()
+for i in range(1, num+1):
+    result[i] = round((1+1/i)**i)
+
+sums = 0
+for i in result:
+    sums += result[i]
+
+print(result)        
